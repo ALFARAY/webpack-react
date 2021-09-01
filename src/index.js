15cbd2fb744b8883ba667239f26dfb9f3c3816d3
Robert Alfaro Yari <33809886+ALFARAY@users.noreply.github.com>
2021-09-01 15:11:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HolaMundo from "./components/HolaMundo";
 
-ReactDOM.render(<HolaMundo />, document.getElementById("app"));
+ReactDOM.render(<React.StrictMode> <HolaMundo /> </React.StrictMode>, document.getElementById("app"));
